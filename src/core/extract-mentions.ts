b845dad0968ccfa42fcb5ed23b33acdeaf278264
otofune <otofune@gmail.com>
2016-07-24 15:53:38 +0000

    check mentions duplicate

--- src/core/extract-mentions.ts
+++ src/core/extract-mentions.ts
@@ -6,7 +6,14 @@ export default function(text: string): Promise<IUser[]> {
 		return Promise.resolve(null);
 	}
 
-	const mentions = text.match(/@[a-zA-Z0-9\-]+/g);
+	var mentions = text.match(/@[a-zA-Z0-9\-]+/g);
+
+	// 重複チェック
+	// http://qiita.com/cocottejs/items/7afe6d5f27ee7c36c61f
+	mentions = mentions.filter(function (x, i, self) {
+			return self.indexOf(x) === i;
+		}
+	);
 
 	if (mentions === null) {
 		return Promise.resolve(null);

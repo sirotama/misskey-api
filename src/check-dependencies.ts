491a9dba0440842b15543307b0758b1cfb7f9e36
otofune <otofune@gmail.com>
2017-01-25 23:39:26 +0900

    check-dependencies: update MongoDB regexp

--- src/check-dependencies.ts
+++ src/check-dependencies.ts
@@ -4,7 +4,7 @@ import {execSync} from 'child_process';
 export default function(): void {
 	checkDependency('Node.js', 'node -v', x => x.match(/^v(.*)\r?\n$/)[1]);
 	checkDependency('npm', 'npm -v', x => x.match(/^(.*)\r?\n$/)[1]);
-	checkDependency('MongoDB', 'mongo --version', x => x.match(/^MongoDB shell version: (.*)\r?\n$/)[1]);
+	checkDependency('MongoDB', 'mongo --version', x => x.match(/^MongoDB shell version:? (.*)\r?\n/)[1]);
 	checkDependency('Redis', 'redis-server --version', x => x.match(/v=([0-9\.]*)/)[1]);
 	logDone('Checked external dependencies');
 }
